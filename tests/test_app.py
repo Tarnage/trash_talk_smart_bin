c5c812ec7d971dc47a105a9ef73cda6ccf317314
Steven Li <stevenli@Stevens-MacBook-Pro.local>
2024-10-14 19:47:31 +0800

    SQLALCHEMY_DATABASE_URI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,15 @@
 import unittest
-from app import app, db, SmartBinData
+from app import create_app, db, SmartBinData
 
 class TestFlaskApp(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Set up the test client and create the database."""
-        cls.app = app.test_client()
-        cls.app_context = app.app_context()
+        cls.app = create_app()
+        cls.app.config['TESTING'] = True
+        cls.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        cls.client = cls.app.test_client()
+        cls.app_context = cls.app.app_context()
         cls.app_context.push()
         db.create_all()
 
@@ -19,7 +22,7 @@ class TestFlaskApp(unittest.TestCase):
 
     def test_index_route(self):
         """Test the index route."""
-        response = self.app.get('/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'SmartBin Flask App is running!', response.data)
 
